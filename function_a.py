f831e8db4e844500225b1b7a70121b44e0765190
Rahel Kemal <rahelskemal@gmail.com>
2022-09-22 10:50:36 -0700

    added more nums to test list

--- function_a.py
+++ function_a.py
@@ -5,5 +5,5 @@ def most_common_value(number_list):
 
 
 if __name__ == "__main__":
-    nums = [1, 1, 3, 3, 3, 7, 8, 2, 1, 3]
+    nums = [1, 1, 3, 3, 3, 7, 8, 2, 1, 3, 4, 6, 7]
     print(f"Most common value = {most_common_value(nums)}")
